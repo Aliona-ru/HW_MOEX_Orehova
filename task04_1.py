025aa38bcbffdd71b7e635adf1ed94be8c5d4f46
Aliona-ru <Aliona.Orehova@moex.com>
2021-11-30 12:20:33 +0300

    Create .py file with HW from 4 lesson

--- task04_1.py
+++ task04_1.py
@@ -16,9 +16,14 @@ import logging
 
 script, Proggy_for_execute = argv
 print('Proggy_for_execute = ', Proggy_for_execute)
-subprocess.run([Proggy_for_execute])
-
-
+try:
+    subprocess.run([Proggy_for_execute])
+except FileNotFoundError:
+    print("Can't fined this proggy {0}!!!".format(Proggy_for_execute))
+except OSError:
+    print("{0} не является приложением Win32 !!!".format(Proggy_for_execute))
+finally:
+    print("be carefully!!!")
 
 
 
